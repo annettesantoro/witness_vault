582be01cc3f49d771070a52f7b3548202707fb7d
Annette Santoro <annette.santoro@gmail.com>
2018-10-02 22:55:55 -0400

    added login stuff

--- core/views.py
+++ core/views.py
@@ -1,30 +1,34 @@
-from django.shortcuts import render
+
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from random import randint
 from django.views.generic import TemplateView
 # Create your views here.
 
+
+# Home #
+########
 def timeout(request):
     return render(request,
                   'core/timeout.html',
                   {'title': 'Time Out'})
-
-def login(request):
-    return render(request,
-                  'core/login.html',
-                  {'title': 'Log In'})                  
 
 def logout(request):
     return render(request,
                   'core/logout.html',
                   {'title': 'Log Out'})
 
+# Home #
+########
 def home(request):
     return render(request,
                   'core/index.html',
                   {'title': 'Home'})
 
+
+@login_required(login_url='/login/')
 def welcome(request):
     return render(request,
                   'core/welcome.html',
